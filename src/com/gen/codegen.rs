37cd45e0257a6e5f10d832d248167291c405ec6a
catapillie <espritox@gmail.com>
2025-02-28 01:34:20 +0100

    wip: gen modules, stmt and expr

--- src/com/gen/codegen.rs
+++ src/com/gen/codegen.rs
@@ -32,8 +32,53 @@ impl<'a> Codegen<'a> {
     }
 
     pub fn gen(&mut self) -> binary::Result<()> {
-        binary::write_opcode(&mut self.cursor, &Opcode::halt)?;
+        for module in self.ir {
+            self.gen_module(module)?;
+        }
         Ok(())
+    }
+
+    fn gen_module(&mut self, module: &'a ir::Module) -> binary::Result<()> {
+        for stmt in &module.stmts {
+            self.gen_statement(stmt)?;
+        }
+        Ok(())
+    }
+
+    fn gen_statement(&mut self, stmt: &'a ir::Stmt) -> binary::Result<()> {
+        use ir::Stmt as S;
+        match stmt {
+            S::Missing => Ok(()),
+            S::Nothing => Ok(()),
+            S::Expr(expr, _) => {
+                self.gen_expression(expr)?;
+                Ok(())
+            }
+            S::Let(_, _) => todo!("let statement"),
+        }
+    }
+
+    fn gen_expression(&mut self, expr: &'a ir::Expr) -> binary::Result<()> {
+        use ir::Expr as E;
+        match expr {
+            E::Missing => Ok(()),
+            E::Int(..) => todo!(),
+            E::Float(..) => todo!(),
+            E::String(..) => todo!(),
+            E::Bool(..) => todo!(),
+            E::Var(..) => todo!(),
+            E::Tuple(..) => todo!(),
+            E::Array(..) => todo!(),
+            E::Block(..) => todo!(),
+            E::Conditional(..) => todo!(),
+            E::Break(..) => todo!(),
+            E::Skip(..) => todo!(),
+            E::Fun(..) => todo!(),
+            E::Call(..) => todo!(),
+            E::Variant(..) => todo!(),
+            E::Record(..) => todo!(),
+            E::Access(..) => todo!(),
+        }
     }
 
     pub fn done(self) -> binary::Result<Vec<u8>> {
